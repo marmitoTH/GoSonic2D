4a2c174efc26c62bf5ae7e9bb94cc151ae0583ab
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-09-13 20:42:50 -0300

    Create Spring Player State

---
+++ scripts/player/states/spring_player_state.gd
@@ -0,0 +1,22 @@
+extends PlayerState
+
+class_name SpringPlayerState
+
+func enter(player: Player):
+	player.is_jumping = false
+	player.is_rolling = false
+	player.set_bounds(0)
+
+func step(player: Player, delta: float):
+	player.handle_gravity(delta)
+	player.handle_acceleration(delta)
+	
+	if player.is_grounded:
+		player.state_machine.change_state("Regular")
+	elif player.velocity.y > 0:
+		player.state_machine.change_state("Air")
+
+func animate(player: Player, _delta: float):
+	player.skin.set_animation_speed(1.5)
+	player.skin.handle_flip(player.input_direction.x)
+	player.skin.set_animation_state(PlayerSkin.ANIMATION_STATES.corkscrew)
