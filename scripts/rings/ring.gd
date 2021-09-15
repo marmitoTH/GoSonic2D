9303dca70434f0d7d286e5e3911c455dc2c1855c
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-09-15 19:02:49 -0300

    Update Ring

--- scripts/rings/ring.gd
+++ scripts/rings/ring.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 onready var sprite = $Sprite
 onready var collider = $Area2D/CollisionShape2D
+onready var score_controller = $ScoreController
 onready var ring_audio = $RingAudio
 
 func collect():
 	ring_audio.play()
 	sprite.visible = false
+	score_controller.add_score()
 	collider.set_deferred("disabled", true)
 
 func _on_Area2D_area_entered(area):
