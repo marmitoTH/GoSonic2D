5261aa529957ec2dfdd4421937c324e1aa5e9b41
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2022-08-04 19:20:00 -0300

    Fix raycast collision from within colliders

--- scripts/general/go_physics.gd
+++ scripts/general/go_physics.gd
@@ -3,13 +3,19 @@ extends Node
 class_name GoPhysics
 
 const EPSILON = 0.001
+const MAX_RAY_ORIGIN_OVERLAPS = 1
 
 static func cast_ray(world: World2D, origin: Vector2, direction: Vector2, length: float, exclude: Array = [], layer: int = 1) -> Dictionary:
 	var destination = origin + direction * length
 	var space_state = world.direct_space_state
 	var result = space_state.intersect_ray(origin, destination, exclude, layer)
-	var penetration = destination.distance_to(result.position) if result else 0
-	return { 'position': result.position, 'normal': result.normal, 'penetration': penetration, 'collider': result.collider } if result else {}
+	var overlaps = space_state.intersect_point(origin, MAX_RAY_ORIGIN_OVERLAPS, exclude, layer)
+
+	if result and overlaps.size() == 0:
+		var penetration = destination.distance_to(result.position)
+		return { 'position': result.position, 'normal': result.normal, 'penetration': penetration, 'collider': result.collider }
+	
+	return {}
 
 static func cast_parallel_rays(world: World2D, origin: Vector2, offset: Vector2, direction: Vector2, length: float, exclude: Array = [], layer: int = 1) -> Dictionary:
 	var right_ray = cast_ray(world, origin + offset, direction, length, exclude, layer)
