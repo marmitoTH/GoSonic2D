4f97b4f3ea73e14b6f4ece0b7f11cd045ddafd46
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2022-08-04 19:21:11 -0300

    Improve global to ground velocity conversion to be more accurate

--- scripts/general/go_utils.gd
+++ scripts/general/go_utils.gd
@@ -2,6 +2,9 @@ extends Node
 
 class_name GoUtils
 
+const SHALLOW_ANGLE = 23
+const HALF_STEEP_ANGLE = 45
+
 static func get_angle_from(normal: Vector2) -> float:
 	var radians = normal.angle_to(Vector2.UP)
 	return -rad2deg(radians)
@@ -12,5 +15,15 @@ static func ground_to_global_velocity(velocity, normal) -> Vector2:
 	return Vector2(x_speed, y_speed)
 
 static func global_to_ground_velocity(velocity, normal) -> Vector2:
-	var x_speed = velocity.y * normal.x - velocity.x * normal.y
+	var x_speed = velocity.x
+	var angle = abs(get_angle_from(normal))
+
+	if angle > SHALLOW_ANGLE:
+		var direction = sign(normal.x)
+
+		if angle < HALF_STEEP_ANGLE:
+			x_speed = velocity.y * 0.5 * direction
+		else:
+			x_speed = velocity.y * direction
+
 	return Vector2(x_speed, 0)
